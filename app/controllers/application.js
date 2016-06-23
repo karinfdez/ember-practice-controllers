63b36cefdcbeb94d2cfff4d07487136ff227c439
Karin Fernandez <karinfdez@yahoo.com>
2016-06-23 14:04:31 -0400

    actions in controller

---
+++ app/controllers/application.js
@@ -0,0 +1,24 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+	prop2: 'test',
+	prop3: true,
+	actions:{
+		enter(val){
+			alert(this.get('prop1'));
+			alert(this.getProperties('prop1','prop2').prop1);
+			alert(val);
+			this.toggleProperty('prop3');
+		},
+		action1(){
+			alert('Application controller action action1');
+		},
+		action2(){
+			alert('Application controller action action2');
+			return true;
+		},
+		action3(val){
+			alert(`Value Passed: ${val}`);
+		}
+	}
+});
